b3f0b371eef693d462a01d0151cf99d67c357eba
Sama Emad <sammaemad@gmail.com>
2026-07-03 17:08:56 +0300

    Initialize project and connect MongoDB

--- src/server.js
+++ src/server.js
@@ -1,9 +1,20 @@
 require("dotenv").config();
 
 const app = require("./app");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log(" MongoDB Connected");
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(" Database Error:");
+    console.log(err);
+  });
