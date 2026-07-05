49bf6b3157a907206f065ebfcedd91ee1266808d
Muhammed Refaat <114253447+MuhammedRefaatMetwally@users.noreply.github.com>
2026-07-05 15:59:56 +0300

    Add auth routes  and use it in app as api/auth

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+import authRoutes from './routes/auth.route.js';
 
 app.use(express.json());
+app.use('/api/auth', authRoutes);
+
 module.exports = app;
---
+++ src/routes/auth.route.js
@@ -0,0 +1,22 @@
+import { loginSchema } from "../validators/auth.validator";
+
+const {
+  register,
+  login,
+  verifyOTP,
+  logout,
+  getProfile,
+  forgotPassword,
+  resetPassword,
+} = require("../controllers/auth.controller");
+
+const router = require("express").Router();
+
+router.post("/register", register);
+router.post("/login", login);
+router.post("/verifyOTP", verifyOTP);
+router.post("/logout", logout);
+router.post("/forgotPassword", forgotPassword);
+router.post("/resetPassword", resetPassword);
+
+export default router;
