d3d9eba47df82b816e1b4728549a1003fbae1ed1
SALIME-78 <h.salim78.dz@gmail.com>
2026-07-11 00:08:53 +0100

    adding products images service feature

---
+++ src/middlewares/upload.js
@@ -0,0 +1,23 @@
+import multer from 'multer';
+import path from 'path';
+
+const storage = multer.diskStorage({});
+
+// Filter to accept images only
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = /jpeg|jpg|png|webp/;
+  const extname = allowedTypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = allowedTypes.test(file.mimetype);
+
+  if (extname && mimetype) {
+    return cb(null, true);
+  } else {
+    cb(new Error('Only images (jpeg, jpg, png, webp) are allowed.'));
+  }
+};
+
+export const uploadMiddleware = multer({
+  storage,
+  fileFilter,
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
+});
