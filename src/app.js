516d41a8f55075c8e018c8b466002fa8da2dec70
martinamaged2002 <martinamaged444@gmail.com>
2026-07-12 02:35:39 +0300

    Implement wishlist feature

--- src/app.js
+++ src/app.js
@@ -3,10 +3,11 @@ const cookieParser = require("cookie-parser");
 const app = express();
 const authRoutes = require("./routes/auth.route");
 const productRoutes = require("./routes/product.route");
+const  wishlistRoutes= require("./routes/wishlist.route");
 
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
-
+app.use("/api/wishlist", wishlistRoutes);
 module.exports = app;
