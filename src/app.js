40bbeab81838471f6737cec653ffbba0f1820f84
mahmoud26796 <mahmoudmrm96@gmail.com>
2026-07-13 06:17:14 +0300

    created the cart router and started using it in the app file

--- src/app.js
+++ src/app.js
@@ -3,19 +3,20 @@ const cookieParser = require("cookie-parser");
 const app = express();
 const authRoutes = require("./routes/auth.route");
 const productRoutes = require("./routes/product.route");
+const cartRoutes = require("./routes/cart.route");
 
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api/auth', authRoutes);
-app.use('/api/products', productRoutes);
-
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/carts", cartRoutes);
 
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    res.status(statusCode).json({
-        success: false,
-        message: err.message || "Internal Server Error",
-    });
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode).json({
+    success: false,
+    message: err.message || "Internal Server Error",
+  });
 });
 
 module.exports = app;
